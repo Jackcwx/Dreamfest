b438ee3afe3be78be39b674cfbbc68b361a077b7
Jack Wilson <jxwilx@gmail.com>
2024-05-29 11:54:26 +1200

    step2

--- server/db/index.ts
+++ server/db/index.ts
@@ -13,3 +13,21 @@ export async function getAllLocations() {
   const locations: unknown[] = await connection('locations').select('*')
   return locations as Location[]
 }
+
+export async function getEventsForDay(day: string) {
+  const events: unknown[] = await connection('events')
+    .join('locations', 'events.location_id', 'locations.id')
+    .select(
+      'events.id as id',
+      'events.day as day',
+      'events.time as time',
+      'events.name as eventName',
+      'events.description as description',
+      'locations.name as locationName',
+      'events.location_id as locationId',
+      'locations.id as location_id',
+      'locations.name as name',
+    )
+    .where({ day })
+  return events as EventWithLocation[]
+}
